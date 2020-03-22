bb32c01a28fca7ffac540019034b52980eb16c42
unknown <mardzien@gmail.com>
2020-03-22 23:21:04 +0100

    first commit

--- sheets.py
+++
@@ -1,9 +0,0 @@
-import os
-import csv
-import openpyxl
-
-dir_name = "data"
-
-sheets = os.listdir(dir_name)
-### loading first element
-workbook = openpyxl.load_workbook(os.path.join('data', sheets[0]))
