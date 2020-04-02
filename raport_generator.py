83e234ef6c7b80815f8f78e92ca9b13a7587f206
mardzien <mardzien@gmail.com>
2020-04-02 13:30:26 +0200

    bugfixing

--- raport_generator.py
+++ raport_generator.py
@@ -1,11 +1,10 @@
-
-import json
-import requests
-import api_senuto
-import numpy as np
 import pandas as pd
 import glob
+import time
 import xlrd
+import api_senuto
+
+file_path = 'data/raport'
 
 
 def get_domains_from_file(file_name):
@@ -19,8 +18,8 @@ def get_domains_from_file(file_name):
             domains.append(line)
     return domains
 
-
-file_path='data/raport'
+"""funkcja zaimportowana z pliku api_senuto pobiera export z aplikacji senuto i zapisuje go do pliku excel
+funkcja poniżej obsługuje listę domen i zwraca wiele plików do podanej ścieżki"""
 
 
 def get_multiple_important_keywords_export(domains, file_path):
@@ -28,27 +27,36 @@ def get_multiple_important_keywords_export(domains, file_path):
         api_senuto.get_important_keywords_export(domain, file_path)
 
 
-# domains = get_domains_from_file('data/domains.txt')
-# get_multiple_important_keywords_export(domains, 'data/raport')
+"""
+Do tej funkcji potrzeba zainstalować moduły:
+numpy, pandas, xlrd?, 
+"""
 
 
 def create_full_report(file_path):
-    files = glob.glob(f'{file_path}*.xlsx')
+    full_time_start = time.time()
+    files = glob.glob(f'{file_path}/*.xlsx')
     wynikowy = pd.DataFrame()
-    wynikowy.to_excel('output.xlsx')
+    wynikowy.to_excel(f'{file_path}/output.xlsx')
     for file in files:
-        name = file[:file.index('.')]
+        start = time.time()
+        name = file[file.index('\\')+1:file.index('.')]
         dataframe = pd.read_excel(file)
         wynikowy = wynikowy.append(dataframe[['Słowo kluczowe', 'Śr. mies. liczba wyszukiwań']])
-        with pd.ExcelWriter('output.xlsx', engine='openpyxl', mode='a') as writer:
+        with pd.ExcelWriter(f'{file_path}/output.xlsx', engine='openpyxl', mode='a') as writer:
             dataframe.to_excel(writer, sheet_name=name)
+        stop = time.time()
+        delta = stop - start
+        print(f"Do raportu dodano plik: {name} w czasie: {delta}")
 
     wynik = wynikowy.drop_duplicates(ignore_index=True)
 
-    with pd.ExcelWriter('output.xlsx', engine='openpyxl', mode='a') as writer:
+    ### dodanie arkusza zbiorczego do pliku output.xlsx
+    with pd.ExcelWriter(f'{file_path}/output.xlsx', engine='openpyxl', mode='a') as writer:
         wynik.to_excel(writer, sheet_name='zbiorczy')
+    full_time_stop = time.time()
+    full_delta = full_time_stop - full_time_start
+    print(f"Wygenerowana cały raport w czasie: {full_delta}")
 
 
-
-
-
+create_full_report(file_path)
